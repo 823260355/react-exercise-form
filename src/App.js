cfad26914fe359780a9fdff860746aa485c4767c
LiYuan1229 <823260355@qq.com>
2020-09-23 19:48:22 +0800

    [liyuan]:finish this form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.less';
 import MyProfile from "./components/MyProfile";
 
+
 const App = () => {
   return (
     <div className='App'>
--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -5,21 +5,26 @@ class MyProfile extends Component {
   state = {
     name:"",
     gender:"",
-    description:""
+    description:"",
+    read: false
   };
 
-  handleFielsChange = (field, event) =>{
+  handleFielsChange = (field, event, checked=false) =>{
     this.setState({
-      [field]:event.target.value,
+      [field]:checked === false ? event.target.value : event.target.checked,
     });
+  }
+
+  formSubmit = (e) => {
+    console.log(this.state);
   }
   render() {
     return (
       <div className="container">
            <h1 >My Profile</h1>
-           <form className="my-form">
+           <form className="my-form" id="myProfile">
               <div className="form-group">
-                 <label htmlFor="name">Name</label>
+                 <label htmlFor="name" style={{fontWeight: 'bold'}}>Name</label>
                  <input
                    type="text"
                    value={this.state.name}
@@ -30,21 +35,27 @@ class MyProfile extends Component {
                  />
               </div>
               <div className="form-group">
-                  <label  htmlFor="gender">Gender</label>
-                    <select className="form-control" id="gender">
+                  <label  htmlFor="gender" style={{fontWeight: 'bold'}}>Gender</label>
+                    <select className="form-control" id="gender" onChange={(e) => this.handleFielsChange('gender', e)}>
                         <option defaultValue="defaultValue">Male</option>
                         <option>Female</option>
                     </select>
               </div>
               <div className="form-group">
-                   <label htmlFor="decription">Decription</label>
-                  <textarea className="form-control" id="decription" rows="3" placeholder="Decription about yourself"></textarea>
+                   <label htmlFor="decription" style={{fontWeight: 'bold'}}>Decription</label>
+                  <textarea className="form-control" id="decription" rows="3" placeholder="Decription about yourself" onChange={(e) => this.handleFielsChange('description', e)}></textarea>
               </div>
               <div className="form-group form-check">
-                   <input type="checkbox" className="form-check-input" id="read"/>
+                   <input type="checkbox" className="form-check-input" id="read" onChange={(e) => this.handleFielsChange('read', e, true)}/>
                    <label className="form-check-label" htmlFor="read">I have read the terms of conduct</label>
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <div className="form-group" style={{textAlign:'center'}}>
+                   <button type="submit" className="btn btn-primary" disabled={
+                        this.state.name === "" ||
+                        this.state.gender === "" || 
+                        this.state.description === "" || 
+                        this.state.read === false} onClick={this.formSubmit()}>Submit</button>
+              </div>       
              </form>
       </div> 
     );
